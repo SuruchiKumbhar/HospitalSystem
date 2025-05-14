0dc8d0ee9961fe0d836e260f08db400596da5801
SuruchiKumbhar <suruchi8999@gmail.com>
2025-05-14 22:48:43 +0530

    Add files via upload

---
+++ AdminClass.java
@@ -0,0 +1,22 @@
+package com.test;
+
+//com.c package imported
+import com.c.*;
+
+// Class AdminClass
+public class AdminClass{
+
+
+// main mthod entry point of every project
+public static void main(String[] args){
+
+ System.out.println("--- main method ---");
+
+//created object of R class
+ R r = new R();
+
+ System.out.println(r.k);
+ r.m3();
+
+}
+}
